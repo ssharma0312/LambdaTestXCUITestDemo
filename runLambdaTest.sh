cae47688dd1bda27bc850f74c91064ab14b29569
Sahil Sharma <sahilsharma@Sahils-MacBook-Pro.local>
2024-04-11 23:44:24 -0600

    updated github yaml

--- runLambdaTest.sh
+++ runLambdaTest.sh
@@ -2,8 +2,8 @@
 #sh prepareTestBuild-rdc.sh
 
 # Configure environment variables
-export LT_USERNAME=$LT_USERNAME
-export LT_ACCESS_KEY=$LT_ACCESS_KEY
+export LT_USERNAME=sam.shar99
+export LT_ACCESS_KEY=uVFcrGeLUwYBUAYkiyA6auIXYPW0YZNlHGdT2gYPsIztDyq3j0
 
 # Allow permissions
 chmod u+x ./LambdaTestFiles/hyperexecute
