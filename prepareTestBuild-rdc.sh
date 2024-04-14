2d93c95c418bc7d702a922f79dc038c1fcb6aefb
Sahil Sharma <sahilsharma@Sahils-MacBook-Pro.local>
2024-04-14 15:30:25 -0600

    updated comments in YML

--- prepareTestBuild-rdc.sh
+++ prepareTestBuild-rdc.sh
@@ -1,31 +1,48 @@
-xcodebuild \
+#!/bin/bash
+
+# Define variables for common paths and settings
+PROJECT_NAME="LambdaTestiOSDemo" # Change to your project name
+SCHEME_NAME="LambdaTestiOSDemo" # Change to your schmeme name
+CONFIGURATION="Debug"
+SDK="iphoneos"
+DERIVED_DATA_PATH="./customFolder-rdc"
+BUILD_PRODUCTS_PATH="$DERIVED_DATA_PATH/Build/Products/$CONFIGURATION-$SDK"
+APPS_TO_TEST_DIR="AppsToTest-rdc"
+
+# Clean and build the project for testing
+xcodebuild clean build-for-testing \
   CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO CODE_SIGNING_ALLOWED=NO \
-  clean build-for-testing \
-  -project LambdaTestiOSDemo.xcodeproj \
-  -scheme "LambdaTestiOSDemo" \
-  -sdk iphoneos \
-  -configuration Debug \
-  -derivedDataPath './customFolder-rdc'
+  -project "$PROJECT_NAME.xcodeproj" \
+  -scheme "$SCHEME_NAME" \
+  -sdk "$SDK" \
+  -configuration "$CONFIGURATION" \
+  -derivedDataPath "$DERIVED_DATA_PATH"
 
+# Navigate to the directory containing build products
+cd "$BUILD_PRODUCTS_PATH"
 
-cd customFolder-rdc/Build/Products/Debug-iphoneos
+# Package the main app into an IPA
 mkdir Payload
-cp -r LambdaTestiOSDemo.app Payload/
-zip --symlinks -r LambdaTestiOSDemo.ipa Payload
-rm -rf Payload; mkdir Payload
-cp -r LambdaTestiOSDemoUITests-Runner.app Payload/
-zip --symlinks -r LambdaTestiOSDemoUITests-Runner.ipa Payload
+cp -r "$PROJECT_NAME.app" Payload/
+zip --symlinks -r "$PROJECT_NAME.ipa" Payload
+rm -rf Payload
 
-cd ../../../..
+# Package the test runner app into an IPA
+mkdir Payload
+cp -r "${PROJECT_NAME}UITests-Runner.app" Payload/
+zip --symlinks -r "${PROJECT_NAME}UITests-Runner.ipa" Payload
+rm -rf Payload
 
-# Check if AppsToTest folder exists, create one if not, and ensure it's empty
-if [ ! -d "AppsToTest-rdc" ]; then
-    mkdir -p AppsToTest-rdc
+# Navigate back to the project root directory
+cd -
+
+# Ensure the AppsToTest directory exists and is empty
+if [ ! -d "$APPS_TO_TEST_DIR" ]; then
+    mkdir -p "$APPS_TO_TEST_DIR"
 else
-    rm -rf AppsToTest-rdc/*
+    rm -rf "$APPS_TO_TEST_DIR/*"
 fi
 
-
-# Copy .ipa to AppToTest-rdc folder
-cp customFolder-rdc/Build/Products/Debug-iphoneos/LambdaTestiOSDemo.ipa AppsToTest-rdc/
-cp customFolder-rdc/Build/Products/Debug-iphoneos/LambdaTestiOSDemoUITests-Runner.ipa AppsToTest-rdc/
+# Copy the IPAs to the AppsToTest directory
+cp "$BUILD_PRODUCTS_PATH/$PROJECT_NAME.ipa" "$APPS_TO_TEST_DIR/"
+cp "$BUILD_PRODUCTS_PATH/${PROJECT_NAME}UITests-Runner.ipa" "$APPS_TO_TEST_DIR/"
